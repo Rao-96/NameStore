7a9c7e99c1d686a16b4dc19b4853223063597d43
Ramith <53828605+Rao-96@users.noreply.github.com>
2022-08-20 10:42:42 +0530

    add db store

---
+++ src/main/java/DbNameStore.java
@@ -0,0 +1,38 @@
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+
+public class DbNameStore implements NameStore {
+    String connectionfileName="http://root:12345678@localhost:3306";
+    String db="namestore";
+    @Override
+    public void storeName(String name){
+//        try{
+//            FileWriter writer=new FileWriter(fileName,true);
+//            writer.append(name+"\n");
+//            writer.close();
+//        }catch(IOException e){
+//            throw new RuntimeException("File can't read"+e);
+//
+//        }
+        //to be done
+
+    }
+    @Override
+    public List<String>getNames(){
+//        try{
+//            return Files.readAllLines(Path.of(fileName));
+//
+//        }catch(RuntimeException | IOException e){
+//            throw new RuntimeException("File can't read"+e);
+//        }
+
+        //to ber done
+        return null;
+    }
+}
